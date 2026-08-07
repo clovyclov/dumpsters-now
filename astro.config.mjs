4494fda250339a77e724a9d7b4b520db4e12fee7
Clovis Shropshire <shropshireclovis@gmail.com>
2026-08-07 17:16:15 -0400

    Configure static 301 redirects for legacy URLs in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,16 @@ export default defineConfig({
   redirects: {
     '/construction-debris-removal': '/construction-debris-dumpster-rentals/',
     '/construction-debris-removal/': '/construction-debris-dumpster-rentals/',
+    '/northville/dumpster-rental': '/service-areas/dumpster-rental-northville-mi/',
+    '/northville/dumpster-rental/': '/service-areas/dumpster-rental-northville-mi/',
+    '/plymouth/dumpster-rental': '/service-areas/dumpster-rental-plymouth-mi/',
+    '/plymouth/dumpster-rental/': '/service-areas/dumpster-rental-plymouth-mi/',
+    '/detroit/dumpster-rental': '/service-areas/dumpster-rental-detroit-mi/',
+    '/detroit/dumpster-rental/': '/service-areas/dumpster-rental-detroit-mi/',
+    '/ann-arbor/dumpster-rental': '/service-areas/',
+    '/ann-arbor/dumpster-rental/': '/service-areas/',
+    '/services/commercial-dumpster-rentals': '/commercial-dumpster-rentals/',
+    '/services/commercial-dumpster-rentals/': '/commercial-dumpster-rentals/',
   },
   integrations: [
     sitemap({
